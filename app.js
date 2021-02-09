e11a701e1feb108842be918ca67ef1e4d72c6f20
Ming Jhang Chen <ec1348@gmail.com>
2021-02-09 17:24:45 +0800

    Add routes practice.

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', (req, res) => {
+    res.send("<h1>This is my first Express project.</h1>");
+});
+
+app.get('/hello', (req, res) => {
+    res.send("<h1>Hello student, this is my first Express project.</h1>");
+});
+
 app.listen(3000, () => {
-    console.log("This application is running on localhost: 3000!");
+    console.log("This application is running on localhost: 3000!");var now = new Date();
+    // convert date to a string in UTC timezone format:
+    console.log(now.toUTCString());
+    // Output: Wed, 21 Jun 2017 09:13:01 GMT
 });
