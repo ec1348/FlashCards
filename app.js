c9bd3c3208dbc2de83b2c5cd6e1914be5030f10f
Ming Jhang Chen <ec1348@gmail.com>
2021-02-17 21:13:08 +0800

    Add variable use the way similar to template literal in Javascript. #{}

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/cards', (req, res) => {
-    res.render("card", {prompt : "Who is buried in Grant's tomb?"});
+    res.render("card", {prompt : "Who is buried in Grant's tomb?", hint : "Think about whose tomb it is."});
 });
 
 app.listen(3000, () => {
